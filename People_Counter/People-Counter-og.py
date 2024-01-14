742efb43f1eb57ecee52b1fa9d6294e58eb32294
shah672 <109067879+shah672@users.noreply.github.com>
2024-01-14 16:41:03 +0530

    Committing changes before setting up LFS

--- People_Counter/People-Counter-og.py
+++ People_Counter/People-Counter-og.py
@@ -11,7 +11,7 @@ model = YOLO("../Yolo-weights/yolov8l.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "dog", "horse", "sheep", "cow","lion", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
               "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
               "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
               "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
